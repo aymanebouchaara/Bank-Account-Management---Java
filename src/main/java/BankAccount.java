09e0581fd66c90f9c62b3bbe93bebf354b346a84
Bouchaara <2420407@cognizant.com>
2025-06-11 12:50:02 +0200

    saved changes

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,3 +1,22 @@
 public class BankAccount {
 
+    private double balance; 
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    //Methods 
+
+
+
+
+    public void setBalance(double balance) {
+        this.balance = balance; 
+    }
+
+    public double getBalance() {
+        return this.balance; 
+    }
+
 }
