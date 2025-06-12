e697f08444dc02556bf123a978718045c95f76a2
Farooq <2417692@cognizant.com>
2025-06-12 10:20:31 +0100

    Get and set balance tests

--- src/test/java/com/bankaccountmanagement/BankAccountTest.java
+++ src/test/java/com/bankaccountmanagement/BankAccountTest.java
@@ -1,6 +1,8 @@
 package com.bankaccountmanagement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,4 +37,19 @@ class BankAccountTest {
         assertEquals(100, account.getBalance(), 0.001);
     }
 
+    @Test
+    @DisplayName("Check that get balance is working")
+    void correctBalanceShowing() {
+        assertEquals(100,account.getBalance(),0.001,"getBalance has failed");    
+    }
+
+    @Test
+    @DisplayName("Check that the correct balance is set")
+    void correctBalanceSet(){
+        double oldBalance = account.getBalance();
+        account.setBalance(200);
+        double newBalance = account.getBalance();
+        assertEquals(200,newBalance,0.001,"Balance is not set correctly");
+        assertNotEquals(oldBalance,newBalance);
+    }
 }
