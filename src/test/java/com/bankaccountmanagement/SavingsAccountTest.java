605c1803ec561ca360c7afc6bd248ccd6236d8fe
Motala11 <m_011@hotmail.co.uk>
2025-06-12 09:50:59 +0100

    Interest rate test added

--- src/test/java/com/bankaccountmanagement/SavingsAccountTest.java
+++ src/test/java/com/bankaccountmanagement/SavingsAccountTest.java
@@ -4,5 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class SavingsAccountTest {
+	private double interestRate;
+	private SavingsAccount savingsAccount;
+	
+	@BeforeEach
+	void setUp() {
+		savingsAccount = new SavingsAccount(100.0);
+	}
     
+	@Test
+	@DisplayName("Interest is added to account")
+	void interestShouldBeAddedToAccount() {
+		savingsAccount.addInterest();
+		assertEquals(105.0, savingsAccount.getBalance(), 0.001);
+	}
 }
